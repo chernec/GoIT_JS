49aaa6e88f88885442966883a69bfc570065b0a8
OKOPC <OKOPC>
2016-03-15 20:58:35 +0200

    pow edd

--- modul_01/01/js/script.js
+++ modul_01/01/js/script.js
@@ -1,9 +1,17 @@
 function pow() {
-  var num = prompt('Введите число');
-  var exp = prompt('Введите степень');
-  var result = num;
+  var num = prompt('Введите число'),
+      exp = prompt('Введите степень'),
+      result = num;
       
       if (exp == 0) {
+        console.log('1');
+      } else if ( exp <= 0) {
+          for (var j = 1; j < Math.abs(exp); j++) {
+           result*=num;
+          }
+          var minpow=1/result;
+        console.log(minpow);
+        
       } else if (num && exp && !isNaN(num) && !isNaN(exp)) {
         for (var i = 1; i < exp; i++) {
           result*=num;
